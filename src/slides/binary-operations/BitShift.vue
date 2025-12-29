2778e2d26a204c548100d6b4457b6b0924ebcba4
Rob Louie <rob.j.louie@gmail.com>
2025-12-29 18:29:59 -0500

    first slides of gameboy stuff

---
+++ src/slides/binary-operations/BitShift.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import BaseSlideTemplate from "@/BaseSlideTemplate.vue";
+</script>
+
+<template>
+  <BaseSlideTemplate>
+
+    <template v-slot:header>
+      Bitwise Shift << and >>
+    </template>
+
+    <template v-slot:default>
+      <div class="is-flex-grow-1 is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+        TODO
+      </div>
+    </template>
+
+  </BaseSlideTemplate>
+</template>
+
+<style scoped>
+pre {
+  width: 8em;
+  text-align: right;
+}
+</style>
--- src/slides/binary-operations/binary-operations.routes.ts
+++ src/slides/binary-operations/binary-operations.routes.ts
@@ -1,10 +1,12 @@
 import BinaryOperationsIntro from "./BinaryOperationsIntro.vue";
 import BitwiseOr from "./BitwiseOr.vue";
 import BitwiseAnd from "./BitwiseAnd.vue";
+import BitShift from "./BitShift.vue";
 
 export const bitwiseOpsRoutes = [
   { slide: BinaryOperationsIntro, notes: [] },
   { slide: BitwiseOr, notes: [] },
   { slide: BitwiseAnd, notes: [] },
+  { slide: BitShift, notes: [] },
 
 ];
