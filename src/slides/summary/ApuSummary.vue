7cd4beabd8933165917e93c54c7e7855c83e1578
Rob Louie <rob.j.louie@gmail.com>
2026-01-17 13:31:40 -0500

    Summary done minus emulator itself

---
+++ src/slides/summary/ApuSummary.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import BaseSlideTemplate from "@/BaseSlideTemplate.vue";
+</script>
+
+<template>
+  <BaseSlideTemplate>
+    <template v-slot:header>
+      APU Every Frame (16.66 ms)
+    </template>
+
+    <template v-slot:default>
+      <div>
+      <h1 class="is-size-4"></h1>
+      <ul>
+        <li>Step all 4 sound function per instruction (6,000 - 9000 times) to update the position in the duty cycle, custom wave, or noise, to produce the desired frequency</li>
+        <li>Sample all 4 sound functions 735 times, resulting in 2940 samples that are averaged together for the final sound</li>
+      </ul>
+      </div>
+    </template>
+  </BaseSlideTemplate>
+</template>
+
+<style scoped>
+ul {
+  list-style: disclosure-closed;
+  margin: 1em;
+  font-size: 2em;
+}
+
+li {
+  margin-bottom: 0.5em;
+}
+</style>
--- src/slides/summary/summary.routes.ts
+++ src/slides/summary/summary.routes.ts
@@ -1,11 +1,13 @@
 import SummaryOpening from "./SummaryOpening.vue";
 import CpuSummary from "./CpuSummary.vue";
 import GpuSummary from "./GpuSummary.vue";
+import ApuSummary from "./ApuSummary.vue";
 
 export const summaryRoutes = [
   { slide: SummaryOpening, notes: [] },
   { slide: CpuSummary, notes: [] },
   { slide: GpuSummary, notes: [] },
+  { slide: ApuSummary, notes: [] },
 
 
 ];
